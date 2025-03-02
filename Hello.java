8566fae9f6c370d72b8b0dc1358d09ecd30c7263
CaioBack06 <161071713+CaioBack06@users.noreply.github.com>
2025-03-02 17:52:54 -0300

    Adicionando método holaQueTal

--- Hello.java
+++ Hello.java
@@ -1,5 +1,19 @@
 public class Hello {
     public static void main (Strinh[] args) {
+        helloWorld();
+        olaMundo();
+        holaQuelTal();
+    }
+
+    public static void helloWorld() {
         System.out.println("Hello World");
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
+    }
+
+    public static void holaQuelTal() {
+        System.out.println("Hola Que Tal");
     }
 }
